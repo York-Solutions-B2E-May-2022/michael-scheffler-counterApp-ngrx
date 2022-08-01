4fa970b21707718a811ae5b043e8d298f5acee38
yorkmacbook058 <lanescheffler@gmail.com>
2022-08-01 12:49:36 -0500

    final commit: app functionality working with material & css changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
+import { CounterButtonsComponent } from './counter-buttons/counter-buttons.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+
+
+
+
 import { AppComponent } from './app.component';
+import { COUNTER_STATE_NAME } from './store/counter.selector';
+import { reducer } from './store/counter.reducer';
+import { CounterOutputComponent } from './counter-output/counter-output.component';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterButtonsComponent,
+    CounterOutputComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forFeature(COUNTER_STATE_NAME,reducer),
+    StoreModule.forRoot({}),
+    BrowserAnimationsModule, 
+    MatButtonModule, 
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/store/counter.selector.ts
@@ -0,0 +1,13 @@
+// import { state } from "@angular/animations";
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+import { CounterState } from "./counter.state";
+
+export const COUNTER_STATE_NAME = 'counter';
+
+export const selectCounter = 
+createFeatureSelector<CounterState>(COUNTER_STATE_NAME);
+
+export const getCount = createSelector(
+    selectCounter, 
+    (state: CounterState) => state.counter
+    );
